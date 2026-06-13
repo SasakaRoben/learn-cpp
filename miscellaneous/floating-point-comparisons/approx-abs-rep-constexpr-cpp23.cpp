7a9218022244fd8de1a38549a7656bb6ed4225b5
SasakaRoben <sasakarobenld@gmail.com>
2026-06-13 09:00:23 +0300

    C++ 23 constexpr version of the floating point comparison functions

---
+++ miscellaneous/floating-point-comparisons/approx-abs-rep-constexpr-cpp23.cpp
@@ -0,0 +1,20 @@
+// C++23 version
+#include <algorithm> // for std::max
+#include <cmath>     // for std::abs (constexpr in C++23)
+
+// Return true if the difference between a and b is within epsilon percent of the larger of a and b
+constexpr bool approximatelyEqualRel(double a, double b, double relEpsilon)
+{
+	return (std::abs(a - b) <= (std::max(std::abs(a), std::abs(b)) * relEpsilon));
+}
+
+// Return true if the difference between a and b is less than or equal to absEpsilon, or within relEpsilon percent of the larger of a and b
+constexpr bool approximatelyEqualAbsRel(double a, double b, double absEpsilon, double relEpsilon)
+{
+    // Check if the numbers are really close -- needed when comparing numbers near zero.
+    if (std::abs(a - b) <= absEpsilon)
+        return true;
+
+    // Otherwise fall back to Knuth's algorithm
+    return approximatelyEqualRel(a, b, relEpsilon);
+}
