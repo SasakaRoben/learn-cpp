8e938c90fd6ea078af8ca7e43793b4923afc1cf2
SasakaRoben <sasakarobenld@gmail.com>
2026-07-06 17:36:30 +0300

    Update Session class to track which letters the user has guessed so far

--- projects/ch16-dynamic-arrays/cpp-hangman/main.cpp
+++ projects/ch16-dynamic-arrays/cpp-hangman/main.cpp
@@ -1,6 +1,7 @@
 #include "Random.h"
 #include <iostream>
 #include <string_view>
+#include <vector>
 
 bool validate_input();
 namespace WorldList {
@@ -16,27 +17,50 @@ namespace WorldList {
 class Session {
 private:
     std::string_view m_word { WorldList::get_random_word() };
+    std::vector<bool> m_letter_guessed { std::vector<bool>(26) };
+
+    std::size_t to_index(char c) const {
+        return static_cast<std::size_t>((c % 32) - 1);
+    }
 
 public:
     std::string_view get_word() const { return m_word; }
+
+    bool is_letter_guessed(char c) const { 
+        return m_letter_guessed[to_index(c)];
+    }
+
+    void set_letter_guessed(char c) {
+        m_letter_guessed[to_index(c)] = true;
+    }
 };
 
-void display_state(const Session& session) {
+void display_state(const Session& session, char guess) {
     std::cout << '\n';
     
     std::cout << "The word: ";
-    for ([[maybe_unused]] auto c : session.get_word()) {
-        std::cout << '_';
+    for (auto c : session.get_word()) {
+        if (guess == c) {
+            std::cout << c;
+        }
+        else {
+            std::cout << '_';
+        }
     }
     std::cout << '\n';
 }
 
-char get_guesses() {
+char get_guesses(Session& session) {
     char input {};
 
     while (true) {        
         std::cout << "Enter you next letter: ";
         std::cin >> input;
+
+        if(session.get_seen_letters()[static_cast<std::size_t>(input % 32) - 1]) {
+            std::cout << "You already guessed that. Try again.\n";
+            continue;
+        }
 
         if (validate_input() && (input >= 'a' && input <= 'z'))
             break;
@@ -44,6 +68,7 @@ char get_guesses() {
         std::cout << "That wasn't a valid input. Try again.\n";
     }
 
+    session.update_seen(input);
     return input;
 }
 
@@ -53,9 +78,23 @@ int main() {
     
     Session session {};
 
-    display_state(session);
-    char c { get_guesses() };
-    std::cout << "You guessed: " << c << '\n';
+    for (auto e : session.get_seen_letters()) {
+        std::cout << e << ' ';
+    }
+
+    std::cout << '\n';
+
+    
+    for (int i {0}; i < 6; ++i) {
+        char c { get_guesses(session) };
+        display_state(session, c);
+    }
+
+    for (auto e : session.get_seen_letters()) {
+        std::cout << e << ' ';
+    }
+
+    std::cout << '\n';
 
 	return 0;
 }
