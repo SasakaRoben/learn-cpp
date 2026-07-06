8f733eff90a1177424a5164e4a682384f07c64c2
SasakaRoben <sasakarobenld@gmail.com>
2026-07-06 04:39:06 +0300

    Define different types of items

---
+++ projects/ch16-dynamic-arrays/game/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <cassert>
+
+namespace Items {
+    enum Type {
+        health_potions,
+        torches,
+        arrows,
+        max_types,
+    };
+};
+
+
