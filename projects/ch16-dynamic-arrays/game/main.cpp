4a2bfb1b6c892936b1308d70c8088d0713635f50
SasakaRoben <sasakarobenld@gmail.com>
2026-07-06 04:39:38 +0300

    Define initial values of the player inventory

--- projects/ch16-dynamic-arrays/game/main.cpp
+++ projects/ch16-dynamic-arrays/game/main.cpp
@@ -11,4 +11,21 @@ namespace Items {
     };
 };
 
+int count_total_items(const std::vector<int>& inventory) {
+    int sum {};
+    for (auto e : inventory) {
+        sum += e;
+    }
 
+    return sum;
+}
+
+int main() {
+    std::vector inventory { 1, 5, 10 };
+    assert(Items::max_types == inventory.size() && "Arrays don't match");
+
+    std::cout << "You have " << count_total_items(inventory) 
+              << " total items\n";
+
+    return 0;
+}
