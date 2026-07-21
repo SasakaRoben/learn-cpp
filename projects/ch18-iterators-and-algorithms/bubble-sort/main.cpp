31198a0157070309518dee646237ce641113064a
sasakar <sasakarobenld@gmail.com>
2026-07-21 05:50:52 +0300

    Account for the fact that the last element is already sorted with each subsequent iteration

--- projects/ch18-iterators-and-algorithms/bubble-sort/main.cpp
+++ projects/ch18-iterators-and-algorithms/bubble-sort/main.cpp
@@ -8,7 +8,12 @@ int main() {
     // Step through each element of the array (except the last, which will
     // already be sorted by the time we get to it)
     for (std::size_t iteration { 0 }; iteration < length-1; ++iteration) {
-        for (std::size_t current_index { 0 }; current_index < length-1; 
+        // Account for the fact that the last element is already sorted with 
+        // each subsequent iteration so our array "ends" one element sooner
+        std::size_t end_of_array_index { length - iteration };
+
+        for (std::size_t current_index { 0 }; 
+             current_index < end_of_array_index; 
              ++current_index) {
             // Search through all the elements up to the end of the array-1
             // The last element has no pair to compare against
