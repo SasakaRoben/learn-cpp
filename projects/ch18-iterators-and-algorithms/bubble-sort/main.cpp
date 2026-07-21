e469be02e97f2290db1639c7298f44a5fede881b
sasakar <sasakarobenld@gmail.com>
2026-07-21 05:57:36 +0300

    Add condition for early termination

--- projects/ch18-iterators-and-algorithms/bubble-sort/main.cpp
+++ projects/ch18-iterators-and-algorithms/bubble-sort/main.cpp
@@ -12,14 +12,28 @@ int main() {
         // each subsequent iteration so our array "ends" one element sooner
         std::size_t end_of_array_index { length - iteration };
 
+        // Keep track of whether any elements were swapped in this iteration
+        bool swapped { false };
+
         for (std::size_t current_index { 0 }; 
-             current_index < end_of_array_index; 
+             current_index < end_of_array_index - 1; 
              ++current_index) {
             // Search through all the elements up to the end of the array-1
             // The last element has no pair to compare against
             if (array[current_index] > array[current_index+1]) {
                 std::swap(array[current_index], array[current_index+1]);
+                swapped = true;
             }
+        }
+
+        // If we haven't swapped any elements this iteration,
+        // we're done early
+        if (!swapped) {
+            // Iteration is 0 based, but counting iterations is 1 based. So
+            // add 1 here to adjust
+            std::cout << "Early termination on iteration: " << iteration+1
+                        << "\n";
+            break;
         }
     }
 
