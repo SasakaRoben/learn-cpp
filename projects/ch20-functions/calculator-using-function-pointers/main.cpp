de4c6eff94e78279c0fe87f8719b0ad0f44afe1a
sasakar <sasakarobenld@gmail.com>
2026-07-24 04:45:38 +0300

    Get user input

---
+++ projects/ch20-functions/calculator-using-function-pointers/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+bool validate_input();
+
+int get_integer() {
+    int x {};
+
+    while (true) {
+        std::cout << "Enter an integer: ";
+        std::cin >> x;
+
+        if (validate_input()) {
+            return x;
+        }
+
+        std::cout << "Invalid input. Try again.\n";
+    }
+}
+
+char get_operator() {
+    char op {};
+
+    while (true) {
+        std::cout << "Enter a mathematical operation (+,-,*,/): ";
+        std::cin >> op;
+        if (validate_input() 
+            && (op == '+'
+            || op == '-'
+            || op == '/'
+            || op == '*') ) {
+            return op;
+        }
+
+        std::cout << "Invalid input. Try again.\n";
+    }
+}
+
+
+int main() {
+    int x { get_integer() };
+    int y { get_integer() };
+    char op { get_operator() };
+
+    std::cout << x << y << op << "\n";
+
+    return 0;
+}
