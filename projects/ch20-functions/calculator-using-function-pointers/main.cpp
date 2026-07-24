953696a4b4b4ad3e61f4e5e0a7586d2372561c98
sasakar <sasakarobenld@gmail.com>
2026-07-24 04:46:08 +0300

    Add arithmetic operations functions

--- projects/ch20-functions/calculator-using-function-pointers/main.cpp
+++ projects/ch20-functions/calculator-using-function-pointers/main.cpp
@@ -35,6 +35,22 @@ char get_operator() {
     }
 }
 
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int divide(int x, int y) {
+    return x / y;
+}
+    
 
 int main() {
     int x { get_integer() };
