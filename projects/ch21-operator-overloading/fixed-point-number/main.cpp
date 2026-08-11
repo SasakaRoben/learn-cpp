1e3cd3e9a6fb304c40dbd7b530b938f88c8293ac
sasakar <sasakarobenld@gmail.com>
2026-08-11 12:14:32 +0300

    Handle the case where the fractional portion is out of bounds
    Decimal overflow is treated as relevant instead of clamping the fraction portion to 99 if >99

--- projects/ch21-operator-overloading/fixed-point-number/main.cpp
+++ projects/ch21-operator-overloading/fixed-point-number/main.cpp
@@ -23,11 +23,20 @@ public:
 				m_decimal = -m_decimal;
 			}
 		}
+
+		// If decimal is out of bounds (in either direction),
+		// adjust the decimal so it's in bounds,
+		// and adjust base to account for the number of units removed from the
+		// decimal
+		m_base += m_decimal / 100;  // integer division
+		m_decimal = m_decimal % 100; // integer remainder
 	}
 
 	explicit operator double() const {
 		return m_base + (static_cast<double>(m_decimal) / 100);
 	}
+
+	friend bool testDecimal(const FixedPoint2 &fp);
 };
 
 // This doesn't require access to the internals of the class, so it can be
@@ -37,26 +46,35 @@ std::ostream& operator<<(std::ostream& out, const FixedPoint2& fp) {
 	return out;
 }
 
-int main() {
-	FixedPoint2 a{ 34, 56 };
+// You will need to make testDecimal a friend of FixedPoint2
+// so the function can access the private members of FixedPoint2
+bool testDecimal(const FixedPoint2 &fp)
+{
+    if (fp.m_base >= 0)
+        return fp.m_decimal >= 0 && fp.m_decimal < 100;
+    else
+        return fp.m_decimal <= 0 && fp.m_decimal > -100;
+}
+
+int main()
+{
+	FixedPoint2 a{ 1, 104 };
 	std::cout << a << '\n';
 	std::cout << static_cast<double>(a) << '\n';
-	assert(static_cast<double>(a) == 34.56);
+	assert(static_cast<double>(a) == 2.04);
+	assert(testDecimal(a));
 
-	FixedPoint2 b{ -2, 8 };
-	assert(static_cast<double>(b) == -2.08);
+	FixedPoint2 b{ 1, -104 };
+	assert(static_cast<double>(b) == -2.04);
+	assert(testDecimal(b));
 
-	FixedPoint2 c{ 2, -8 };
-	assert(static_cast<double>(c) == -2.08);
+	FixedPoint2 c{ -1, 104 };
+	assert(static_cast<double>(c) == -2.04);
+	assert(testDecimal(c));
 
-	FixedPoint2 d{ -2, -8 };
-	assert(static_cast<double>(d) == -2.08);
-
-	FixedPoint2 e{ 0, -5 };
-	assert(static_cast<double>(e) == -0.05);
-
-	FixedPoint2 f{ 0, 10 };
-	assert(static_cast<double>(f) == 0.1);
+	FixedPoint2 d{ -1, -104 };
+	assert(static_cast<double>(d) == -2.04);
+	assert(testDecimal(d));
 
 	return 0;
 }
